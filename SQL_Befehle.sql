93ea82bc59d2150bb89e2d36e3fa9fc9887c0082
housnizalam <109956808+housnizalam@users.noreply.github.com>
2022-11-11 10:09:06 +0100

    s

--- SQL_Befehle.sql
+++ SQL_Befehle.sql
@@ -1,5 +1,5 @@
 /* SQL :http://localhost/phpmyadmin*/
-ich habe Tabelle von Teilnehmers der heßt : Teilnehmers
+ich habe Tabelle von Teilnehmers der heißt : Teilnehmers
 diese Tabelle hat sieben Spalten
 id,name,vorname,alter,adresse,email,noten,nationalität
 __________________________________________________________
@@ -18,9 +18,9 @@ INSERT INTO Teilnehmers (id,name,vorname,alter,adresse,email,noten,nationalität
 INSERT INTO Teilnehmers (id,name,vorname,alter,adresse,email,noten)VALUES(NULL,'z','h',20,'Kasseler Straße 30','z.h.15@gmail.com',75,'Syrer' ),(NULL,'z','h',20,'Kasseler Straße 30','z.h.15@gmail.com',75 ,'Deutscher'),(NULL,'z','h',20,'Kasseler Straße 30','z.h.15@gmail.com',75,'Costarica' )
                                                             /* so kann man mehrere Teinehmern einfügen*/
 UPDATE Teilnehmers SET alter =alter+1 WHERE alter=15        /* so kann ich die informationen von Teinehmern ändern : in diesem Beispiel werden die alle Teinehmern die 15 Jahr alt sind ein Jahr grosser*/
-UPDATE Teilnehmers SET noten =noten+1 WHERE vorname=housni  /* so bekommen die alle Teinehmern die housni heißen 10 noten mehr ;-) */
-SELECT DISTINCT name FROM nationalität                      /* es vermeidet die abermalige Ergebnisse : DISTINCT */
-DELETE FROM Teilnehmers WHERE noten < 40                    /* es löscht die Teinehmern deren Noten kleiner als 40 sind */
+UPDATE Teilnehmers SET noten =noten+1 WHERE vorname='housni'  /* so bekommen die alle Teinehmern die housni heißen 10 noten mehr ;-) */
+SELECT DISTINCT nationalität FROM Teilnehmers                      /* es vermeidet die abermalige Ergebnisse : DISTINCT */
+DELETE * FROM Teilnehmers WHERE noten < 40                    /* es löscht die Teinehmern deren Noten kleiner als 40 sind */
 SELECT MAX (alter) FROM Teilnehmers                         /* es gibt uns der älterste Teilnehmer */
 SELECT MAX (noten) FROM Teilnehmers                         /* es gibt uns der beste Teilnehmer nach noten */
 SELECT AVG (alter) FROM Teilnehmers                         /* es gibt uns den durchnitlicher Alter von Teinehmern */
@@ -33,18 +33,20 @@ SELECT * FROM Teilnehmers WHERE name LIKE '%s%'             /* es gibt uns die a
 SELECT * FROM Teilnehmers WHERE name LIKE '_s%'            /* es gibt uns die alle Teilnehmern deren zweitene Buchstab ihrer Namen s ist */
 SELECT * FROM Teilnehmers WHERE name LIKE 's_%'            /* es gibt uns die alle Teilnehmern deren Namen mit s anfangen und haben mindestens andere Buchstab*/
 SELECT * FROM Teilnehmers WHERE name LIKE 's__%'           /* es gibt uns die alle Teilnehmern deren Namen mit s anfangen und haben mindestens zwei andere Buchstaben*/
-SELECT * FROM Teilnehmers WHERE name LIKE 's%n'            /* es gibt uns die alle Teilnehmern deren Namen mit s anfangen und mit n enden */
+SELECT * FROM Teilnehmers WHERE name LIKE 'sa%n'            /* es gibt uns die alle Teilnehmern deren Namen mit s anfangen und mit n enden */
 SELECT * FROM Teilnehmers WHERE nationalität IN ('Deutsch', 'Turkisch') /* es gibt uns die alle Teilnehmern deren Nationalitäten Deutsch oder Turkisch sind */
+SELECT * FROM Teilnehmers WHERE name IN ('jgsf', 'frg','wrgt','swergt')
 SELECT * FROM Teilnehmers WHERE noten BETWEEN 40 AND 50    /* es gibt uns die alle Teilnehmern deren Noten zwischen 40 und 50 sind */
 SELECT alter , SUM(noten) FROM Teilnehmers GROUP BY alter  /* es gibt uns sie Summe von noten für jedes alter */
 SELECT name , SUM(noten) FROM Teilnehmers GROUP BY name    /* es gibt uns sie Summe von noten für jedes name */
-SELECT name , SUM(noten) AS notensumme FROM Teilnehmers GROUP BY name HAVING notensumme >49  /* es gibt uns die Namen deren notensumme grosser als 49 (bestanden) : AS schaft eine neue Liste deren name notensumme ist aber diese Liste temporar ist */
+SELECT name , AVG(noten) AS notensumme FROM Teilnehmers GROUP BY id HAVING notensumme >49  /* es gibt uns die Namen deren notensumme grosser als 49 (bestanden) : AS schaft eine neue Liste deren name notensumme ist aber diese Liste temporar ist */
 SELECT Teilnehmers.noten , IFNULL(Teilnehmers.noten,0) as noten FROM Teilnehmers /* fals noten = null (der teilnehmer ist zur Prüfung nicht gekommen) dann noten =0 */
 SELECT Teilnehmers.noten , COALESCE(Teilnehmers.noten,0) as noten FROM Teilnehmers /* fals noten = null (der teilnehmer ist zur Prüfung nicht gekommen) dann noten =0 */
 /* Das Unterschied zwischen Primary und Unique ist : erstens nur eine Spalte des Tabels kann Primay werden aber viel Spalte können Unique sein zweitens Primary kann nicht Null sein aber Unique kann */
-/*SELECT marken.* , autos.hersteller FROM autos , marken WHERE marken.m_hersteller=autos.id*/
-/*SELECT marken.* , autos.hersteller FROM marken
-INNER JOIN autos on marken.m_hersteller=autos.id*/
+SELECT marken.name , autos.hersteller FROM autos , marken WHERE marken.m_hersteller=autos.id
+
+SELECT marken.* , autos.hersteller FROM marken
+INNER JOIN autos on marken.m_hersteller=autos.id
 
 TINYINT : - 128 => 127 
 SMALLINT : -32768 => 32767
@@ -53,9 +55,9 @@ INT : -2147483648 => 2147483647
 /* es ist total wichtig diese werte zubenutzen um die grosse der Databank so klein wie öglich zu machen weil grosse kostet Geld*/
 /* Vergleich zwichen VARCHAR und CHAR als Datatype : 
 VARCHAR benutzt dynamic Memory das heißt es konsumiert nur die grosse die er wirklich braucht 
-aber der CHAR benutzt static Memory konsumiert die gantze mögliche Memory die er hat 
+aber der CHAR benutzt static Memory dre konsumiert die gantze mögliche Memory die er hat 
 andereseits CHAR ist viel schneller als VARCHAR weil static Memory viel schneller als dynamic ist 
-Wenn man weißt gennau die Grosse die man braucht dann ist CHAR besser sondern ist VARCHAR besser*/
+Wenn man weißt gennau die Grosse die man braucht dann ist CHAR besser ansonsten ist VARCHAR besser*/
 /* Vergleich zwichen ENUM und SET als Datatype : 
 Die Beide sind wie eine drop List ... aber mit ENUM kann man nur eine Wahl wählen aber mit SET kann man mehrere*/
 
@@ -70,7 +72,7 @@ CREATE TABLE items (
     i_name varchar(255) not NULL ,
     i_price float ,
     i_category int (11)
-    /* nach dem Spalten können wir Primary KEY und FOREIGN KEY wenn nötig erstellen */
+    /* nach den Spalten können wir Primary KEY und FOREIGN KEY wenn nötig erstellen */
     Primary KEY (i_id)
     FOREIGN KEY (i_category) REFERENCES categories (c_id), 
   );
